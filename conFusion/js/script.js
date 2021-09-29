1c1f799db740413b6f835b224a9a61a37140657f
timzo31 <69592997+timzo31@users.noreply.github.com>
2021-09-30 02:12:05 +0800

    End assignment 4

--- conFusion/js/script.js
+++ conFusion/js/script.js
@@ -1,7 +1,27 @@
 "use strict";
 
-const dataAttribs = document.querySelectorAll("[data-]");
+// const remBtnLog = document
+//   .querySelector(".btnLog")
+//   .removeAttribute("data-toggle");
+// const remBtnReserv = document
+//   .querySelector(".btnReserv")
+//   .removeAttribute("data-toggle");
 
-for (let i = 0; i < dataAttribs.length; i++) {
-  console.log(i);
-}
+// document.querySelector(".btnReserv").addEventListener("click", function () {
+//   document.querySelector("#reservationModal").
+// });
+$(document).ready(function () {
+  // data-toggle buttons removed as soon as the document is loaded in line 1 & 2.
+  $(".btnReserv").removeAttr("data-toggle");
+  $("#btnLog").removeAttr("data-toggle");
+
+  // show the reservation modal once the "reserve table" button trigerred
+  $(".btnReserv").click(function () {
+    $("#reservationModal").modal();
+  });
+
+  // shows the login modal once the "login" button is clicked.
+  $("#btnLog").click(function () {
+    $("#loginModal").modal();
+  });
+});
